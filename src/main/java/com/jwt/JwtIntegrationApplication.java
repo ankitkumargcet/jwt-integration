e1d648f34783f82cd0a1a0db2e7e137bc367bf62
Ankit Kumar <ankitkumargcet@gmail.com>
2021-07-24 16:03:27 +0530

    initial commit

---
+++ src/main/java/com/jwt/JwtIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtIntegrationApplication.class, args);
+	}
+
+}
